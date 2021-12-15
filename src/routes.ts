58330dcb3b47bafd65499089ebc1d8addd27b0ca
Luiz Henrique <Luiz_HB@live.com>
2021-12-15 16:01:46 -0300

    add put

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { CreateCategoryController } from "./controllers/CreateCategoryController";
 import { DeleteCategoryController } from "./controllers/DeleteCategoryController";
 import { GetallCategoriesController } from "./controllers/GetAllCategoryController";
+import { UpdateCategoryController } from "./controllers/UpdateCategoryController";
 import { GetallCategoriesService } from "./services/GetAllCategoryService";
 
 const routes = Router();
@@ -9,5 +10,6 @@ const routes = Router();
 routes.post("/categories", new CreateCategoryController().handle);
 routes.get("/categories", new GetallCategoriesController().handle);
 routes.delete("/categories/:id", new DeleteCategoryController().handle)
+routes.put("/categories/:id", new UpdateCategoryController().handle)
 
 export{routes}
